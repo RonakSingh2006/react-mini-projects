a257c097c277d41ec848e8bf09cb65c330b001f2
RonakSingh2006 <ronaksingh201106@gmail.com>
2025-07-08 13:09:21 +0530

    added icons

--- Todo App/src/Components/AddTodo.jsx
+++ Todo App/src/Components/AddTodo.jsx
@@ -1,38 +1,49 @@
+import { useState } from "react";
+import { BiAddToQueue } from "react-icons/bi";
 
-import {useState} from "react";
-
-
-function AddTodo({handleAdd}) {
-
-  let [inputTask , setText] = useState("");
-  let [inputDate , setDate] = useState("");
+function AddTodo({ handleAdd }) {
+  let [inputTask, setText] = useState("");
+  let [inputDate, setDate] = useState("");
 
   return (
     <div className="container text-center">
-      
       <div className="row">
         <div className="col-6">
-          <input type="text" placeholder="Enter Todo" value = {inputTask} onChange = {(e)=>{
-            setText(e.target.value);
-          }}/>
+          <input
+            type="text"
+            placeholder="Enter Todo"
+            value={inputTask}
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+          />
         </div>
 
         <div className="col-4">
-          <input type="date" name="todo-date" id="todo-date" value = {inputDate} onChange={(e)=>{
-            setDate(e.target.value);
-          }}/>
+          <input
+            type="date"
+            name="todo-date"
+            id="todo-date"
+            value={inputDate}
+            onChange={(e) => {
+              setDate(e.target.value);
+            }}
+          />
         </div>
 
         <div className="col-2">
-          <button type="button" className="btn btn-success" onClick = {()=>{
-            handleAdd(inputTask,inputDate);
-            setText("");
-            setDate("");
-          }}>
-            Add
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={() => {
+              handleAdd(inputTask, inputDate);
+              setText("");
+              setDate("");
+            }}
+          >
+            Add <BiAddToQueue />
           </button>
         </div>
-
       </div>
     </div>
   );
--- Todo App/src/Components/TodoItem.jsx
+++ Todo App/src/Components/TodoItem.jsx
@@ -1,12 +1,17 @@
-function TodoItem({ task, date , handleDelete }) {
+import { MdDelete } from "react-icons/md";
+function TodoItem({ task, date, handleDelete }) {
   return (
     <div className="container text-center">
       <div className="row">
         <div className="col-6">{task}</div>
         <div className="col-4">{date}</div>
         <div className="col-2">
-          <button type="button" className="btn btn-danger" onClick = {handleDelete}>
-            Delete
+          <button
+            type="button"
+            className="btn btn-danger"
+            onClick={handleDelete}
+          >
+            Delete <MdDelete />
           </button>
         </div>
       </div>
